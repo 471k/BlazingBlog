7acc58282d3c56469018c3a3f317ed3b14e82878
alik <42393919+471k@users.noreply.github.com>
2025-04-21 20:42:16 +0200

    Created Article service and interface and some stuff

--- BlazingBlog.Application/Articles/ArticleService.cs
+++ BlazingBlog.Application/Articles/ArticleService.cs
@@ -7,26 +7,20 @@ using System.Threading.Tasks;
 
 namespace BlazingBlog.Application.Articles
 {
+    
     public class ArticleService : IArticleService
     {
-        public List<Article> GetAllArticles()
-        {
-            return new List<Article>
-            {
-                new Article
-                {
-                    Id = 1,
-                    Title = "My first article",
-                    Content = "This is my first article"
-                },
+        private readonly IArticleRepository _articleRepository;
 
-                new Article
-                {
-                    Id = 2,
-                    Title = "My second article",
-                    Content = "This is my second article"
-                }
-            };
+        public ArticleService(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
         }
+
+        public async Task<List<Article>> GetAllArticlesAsync()
+        {
+            return await _articleRepository.GetAllArticlesAsync();
+        }
+
     }
 }
---
+++ BlazingBlog.Infrastructure/Repositories/ArticleRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlazingBlog.Domain.Articles;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingBlog.Infrastructure.Repositories
+{
+    internal class ArticleRepository : IArticleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticleRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Article>> GetAllArticlesAsync()
+        {
+            return await _context.Articles.ToListAsync();
+        }
+    }
+}
