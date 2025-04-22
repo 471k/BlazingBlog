f3cff37f497e1b5565f1868492129ea8a6e48b90
alik <42393919+471k@users.noreply.github.com>
2025-04-22 09:29:44 +0200

    Implemented GetArticles with CQRS & MediatR

---
+++ BlazingBlog.Application/Articles/GetArticles/GetArticlesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using BlazingBlog.Domain.Articles;
+using MediatR;
+
+namespace BlazingBlog.Application.Articles.GetArticles
+{
+    public class GetArticlesQueryHandler(IArticleRepository articleRepository) : IRequestHandler<GetArticlesQuery, List<Article>>
+    {
+        private readonly IArticleRepository _articleRepository = articleRepository;
+
+        /*public GetArticlesQueryHandler(IArticleRepository articleRepository)
+        {
+            _articleRepository = articleRepository;
+        }            */
+
+        public async Task<List<Article>> Handle(GetArticlesQuery request, CancellationToken cancellationToken)
+        {
+            var articles = await _articleRepository.GetAllArticlesAsync();
+            return articles;
+        }
+    }
+}
